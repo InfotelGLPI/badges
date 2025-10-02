569270fd12f4004bb033aa1bf07c5cab047728fe
Thetsmr <thetsmr@gmail.com>
2025-10-02 09:27:37 +0200

    Rename files & Bump version

--- setup.php
+++ setup.php
@@ -38,7 +38,7 @@ use GlpiPlugin\Resources\Resource;
 
 global $CFG_GLPI;
 
-define('PLUGIN_BADGES_VERSION', '3.1.0');
+define('PLUGIN_BADGES_VERSION', '3.1.1');
 
 if (!defined("PLUGIN_BADGES_DIR")) {
     define("PLUGIN_BADGES_DIR", Plugin::getPhpDir("badges"));
